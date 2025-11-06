e3411cb22bdf603b8d876f1551458cfc8ea23bdf
gitAly-byte <gitaly2433110@gmail.com>
2025-11-06 17:24:54 +0800

    GROUP 4 CREATED ENROLLMENT TABLE

--- schema.sql
+++ schema.sql
@@ -3,3 +3,4 @@ USE school_db;
 CREATE TABLE students (student_id INT PRIMARY KEY, student_name VARCHAR(100), grade_level VARCHAR(10));
 CREATE TABLE teachers (teacher_id INT PRIMARY KEY, teacher_name VARCHAR(100), subject VARCHAR(50));
 CREATE TABLE  classes (class_id INT PRIMARY KEY, class_name VARCHAR (50), room_no VARCHAR(10));
+CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, class_id INT, FOREIGN KEY (student_id) REFERENCES students(student_id), FOREIGN KEY (class_id) REFERENCES classes(class_id));
