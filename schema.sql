f0e40fc5f71f91f286620dca2c9cd1f6067e78da
tnchzrvgr <vergaratoni783@gmail.com>
2025-11-06 17:29:55 +0800

    Group 5 created Grades table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE students (student_id INT PRIMARY KEY, student_name VARCHAR(100), gr
 CREATE TABLE teachers (teacher_id INT PRIMARY KEY, teacher_name VARCHAR(100), subject VARCHAR(50));
 CREATE TABLE  classes (class_id INT PRIMARY KEY, class_name VARCHAR (50), room_no VARCHAR(10));
 CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, class_id INT, FOREIGN KEY (student_id) REFERENCES students(student_id), FOREIGN KEY (class_id) REFERENCES classes(class_id));
+CREATE TABLE grades (grade_id INT PRIMARY KEY, student_id INT, subject VARCHAR(50), grade FLOAT, FOREIGN KEY (student_id) REFERENCES students(student_id);
