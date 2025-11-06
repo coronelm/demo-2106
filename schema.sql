dafda2cf30d02fc51681a7c7d2b9f89cc4583e41
liragnmc <liragnmc@gmail.com>
2025-11-06 17:14:00 +0800

    Group 2 created teachers table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db;
 USE school_db;
 CREATE TABLE students (student_id INT PRIMARY KEY, student_name VARCHAR(100), grade_level VARCHAR(10));
+CREATE TABLE teachers (teacher_id INT PRIMARY KEY, teacher_name VARCHAR(100), subject VARCHAR(50));
