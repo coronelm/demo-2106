e1c818e0b90993e2408dee461b48e02ce6e640ab
mpuri-17 <24-30735@g.batstate-u.edu.ph>
2025-11-06 17:19:28 +0800

    GROUP 3 CREATED CLASSES TABLE

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;
 USE school_db;
 CREATE TABLE students (student_id INT PRIMARY KEY, student_name VARCHAR(100), grade_level VARCHAR(10));
 CREATE TABLE teachers (teacher_id INT PRIMARY KEY, teacher_name VARCHAR(100), subject VARCHAR(50));
+CREATE TABLE  classes (class_id INT PRIMARY KEY, class_name VARCHAR (50), room_no VARCHAR(10));
