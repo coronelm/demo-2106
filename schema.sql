a5feea5dd5c1800c8d87c6540d1b237e44d3a072
24-39885-hub <24-39885@g.batstate-u.edu.ph>
2025-11-06 17:44:30 +0800

    Group 7 Created Subjects Table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,4 @@ CREATE TABLE  classes (class_id INT PRIMARY KEY, class_name VARCHAR (50), room_n
 CREATE TABLE enrollment (enrollment_id INT PRIMARY KEY, student_id INT, class_id INT, FOREIGN KEY (student_id) REFERENCES students(student_id), FOREIGN KEY (class_id) REFERENCES classes(class_id));
 CREATE TABLE grades (grade_id INT PRIMARY KEY, student_id INT, subject VARCHAR(50), grade FLOAT, FOREIGN KEY (student_id) REFERENCES students(student_id);
 CREATE TABLE department (department_id INT PRIMARY KEY, department_name VARCHAR (50))
+CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name VARCHAR (50), department_id INT, FOREIGN KEY (department_id) REFERENCES department (department_id));
